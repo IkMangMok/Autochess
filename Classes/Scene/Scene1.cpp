a372d074e8b04ab2b9f86c3bdf4f658683e099ce
精神测绘人 <80401491+ikmangmok@users.noreply.github.com>
2021-05-15 14:24:34 +0800

    更新场景

--- Classes/Scene/Scene1.cpp
+++ Classes/Scene/Scene1.cpp
@@ -1,3 +1,17 @@
-#include<iostream>
-using namespace std;
-;
+#include"scene1.h"
+USING_NS_CC;
+
+Scene* scene1::createScene()
+{
+    return scene1::create();
+}
+
+bool scene1::init()
+{
+    return true;
+}
+
+void scene1::menuCloseCallback(cocos2d::Ref* pSender)
+{
+    Director::getInstance()->end();
+} 
--- Classes/Scene/scene1.h
+++ Classes/Scene/scene1.h
@@ -2,3 +2,11 @@
 #include"cocos2d.h"
 #include"AutoChessScene.h"
 #include"Player/Player.h"
+
+class scene1 : public cocos2d::Scene {
+public:
+	static cocos2d::Scene* createScene();
+	virtual bool init();
+	void menuCloseCallback(cocos2d::Ref* pSender);
+	CREATE_FUNC(scene1);
+};
